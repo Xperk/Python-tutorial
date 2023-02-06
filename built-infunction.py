514d0cb9f0e1a087459ddbef12611087be9aad43
Tung <TungLV.B20PT185@stu.ptit.edu.vn>
2023-02-06 20:41:41 +0700

    update

--- built-infunction.py
+++ built-infunction.py
@@ -1,68 +1,133 @@
-# abs()         	Trả về giá trị tuyệt đối của một số
-# all()         	Trả về True khi tất cả các phần tử trong iterable là đúng
-# any()         	Kiểm tra bất kỳ phần tử nào của iterable là True
-# ascii()           Tả về string chứa đại diện(representation) có thể in
-# bin()         	Chuyển đổi số nguyên sang chuỗi nhị phân
-# bool()            Chuyển một giá trị sang Boolean
-# bytearray()       Trả về mảng kích thước byte được cấp
-# bytes()           Trả về đối tượng byte không đổi
-# callable()        Kiểm tra xem đối tượng có thể gọi hay không
-# chr()         	Trả về một ký tự(một chuỗi) từ Integer
-# classmethod()     Trả về một class method cho hàm
-# compile()         Trả về đối tượng code Python
-# complex()         Tạo một số phức
-# delattr()         Xóa thuộc tính khỏi đối tượng
-# dict()            Tạo Dictionary
-# dir()         	Trả lại thuộc tính của đối tượng
-# divmod()          Trả về một Tuple của Quotient và Remainder
-# enumerate()       Trả về đối tượng kê khai
-# eval()            Chạy code Python trong chương trình
-# exec()            Thực thi chương trình được tạo động
-# filter()          Xây dựng iterator từ các phần tử True
-# float()           Trả về số thập phân từ số, chuỗi
-# format()          Trả về representation được định dạng của giá trị
-# frozenset()       Trả về đối tượng frozenset không thay đổi
-# getattr()         Trả về giá trị thuộc tính được đặt tên của đối tượng
-# globals()         Trả về dictionary của bảng sumbol toàn cục hiện tại
-# hasattr()         Trả về đối tượng dù có thuộc tính được đặt tên hay không
-# hash()            Trả về giá trị hash của đối tượng
-# help()            Gọi Help System được tích hợp sẵn
-# hex()         	Chuyển Integer thành Hexadecimal
-# id()          	Trả về định danh của đối tượng
-# input()           Đọc và trả về chuỗi trong một dòng
-# int()         	Trả về số nguyên từ số hoặc chuỗi
-# isinstance()      Kiểm tra xem đối tượng có là Instance của Class không
-# issubclass()      Kiểm tra xem đối tượng có là Subclass của Class không
-# iter()            Trả về iterator cho đối tượng
-# len()         	Trả về độ dài của đối tượng
-# list()            Tạo list trong Python
-# locals()          Trả về dictionary của bảng sumbol cục bộ hiện tại
-# map()         	Áp dụng hàm và trả về một list
-# max()         	Trả về phần tử lớn nhất
-# memoryview()      Trả về chế độ xem bộ nhớ của đối số
-# min()         	Trả về phần tử nhỏ nhất
-# next()            Trích xuất phần tử tiếp theo từ Iterator
-# object()          Tạo một đối tượng không có tính năng(Featureless Object)
-# oct()         	Chuyển số nguyên sang bát phân
-# open()            Trả về đối tượng File
-# ord()         	Trả về mã Unicode code cho ký tự Unicode
-# pow()         	Trả về x mũ y
-# print()           In đối tượng được cung cấp
-# property()        Trả về thuộc tính property
-# range()           Trả về chuỗi số nguyên từ số bắt đầu đến số kết thúc
-# repr()            Trả về representation có thể in của đối tượng
-# reversed()        Trả về iterator đảo ngược của một dãy
-# round()           Làm tròn số thập phân
-# set()         	Tạo một set các phần tử mới
-# setattr()         Đặt giá trị cho một thuộc tính của đối tượng
-# slice()           Cắt đối tượng được chỉ định bằng range()            
-# sorted()          Trả về list được sắp xếp
-# staticmethod()    Tạo static method từ một hàm
-# str()         	Chuyển đối tượng đã cho thành chuỗi
-# sum()         	Thêm một mục vào Iterable
-# super()           Cho phép tham chiếu đến Parent Class bằng super
-# tuple()           Tạo một Tuple
-# type()            Trả về kiểu đối tượng
-# vars()            Trả về thuộc tính __dict__ của class
-# zip()         	Trả về Iterator của Tuple
-# __import__()      Hàm nâng cao, được gọi bằng import
+# abs()              Trả về giá trị tuyệt đối của một số
+
+# all()              Trả về True nếu tất cả các mục trong một đối tượng có thể lặp lại là true
+
+# any()              Trả về True nếu bất kỳ mục nào trong đối tượng có thể lặp lại là true
+
+# ascii()            Trả về phiên bản có thể đọc được của một đối tượng. Thay thế các ký tự không có mã ascii bằng ký tự thoát
+
+# bin()              Trả về phiên bản nhị phân của một số
+
+# bool()             Trả về giá trị boolean của đối tượng được chỉ định
+
+# bytearray()        Trả về một mảng byte
+
+# bytes()            Trả về đối tượng bytes
+
+# callable()         Trả về True nếu đối tượng được chỉ định có thể gọi được, nếu không thì False
+
+# chr()              Trả về một ký tự từ mã Unicode đã chỉ định.
+
+# classmethod()      Chuyển đổi một phương thức thành một phương thức lớp
+
+# compile()          Trả về nguồn được chỉ định dưới dạng một đối tượng, sẵn sàng để được thực thi
+
+# complex()             Trả về một số phức
+
+# delattr()          Xóa thuộc tính đã chỉ định (thuộc tính hoặc phương thức) khỏi đối tượng đã chỉ định
+
+# dict()             Trả về một từ điển (Mảng)
+
+# dir()              Trả về danh sách các thuộc tính và phương thức của đối tượng đã chỉ định
+
+# divmod()           Trả về thương và số dư khi đối số 1 chia cho đối số 2
+
+# enumerate()        Lấy một bộ sưu tập (ví dụ: một tuple) và trả về nó dưới dạng một đối tượng liệt kê
+
+# eval()             Đánh giá và thực thi một biểu thức
+
+# exec()             Thực thi mã (hoặc đối tượng) đã chỉ định
+
+# filter()           Sử dụng chức năng lọc để loại trừ các mục trong một đối tượng có thể lặp lại
+
+# float()            Trả về một số dấu chấm động
+
+# format()           Định dạng một giá trị cụ thể
+
+# Frozenset()        Trả về một đối tượng Frozenset
+
+# getattr()          Trả về giá trị của thuộc tính được chỉ định (thuộc tính hoặc phương thức)
+
+# globals()          Trả về bảng ký hiệu toàn cầu hiện tại dưới dạng từ điển
+
+# hasattr()          Trả về True nếu đối tượng được chỉ định có thuộc tính được chỉ định (thuộc tính/phương thức)
+
+# hash()             Trả về giá trị băm của một đối tượng được chỉ định
+
+# help()             Thực thi hệ thống trợ giúp tích hợp
+
+# hex()              Chuyển đổi một số thành giá trị thập lục phân
+
+# id()               Trả về id của một đối tượng
+
+# input()            Cho phép người dùng nhập liệu
+
+# int()              Trả về một số nguyên
+
+# isinstance()       Trả về True nếu một đối tượng đã chỉ định là một thể hiện của một đối tượng đã chỉ định
+
+# issubclass()       Trả về True nếu một lớp đã chỉ định là một lớp con của một đối tượng đã chỉ định
+
+# iter()             Trả về một đối tượng iterator
+
+# len()              Trả về chiều dài của một đối tượng
+
+# list()             Trả về một danh sách
+
+# locals()           Trả về từ điển cập nhật của bảng ký hiệu cục bộ hiện tại
+
+# map()              Trả về iterator đã chỉ định với chức năng đã chỉ định được áp dụng cho từng mục
+
+# max()              Trả về mục lớn nhất trong một lần lặp
+
+# memoryview()       Trả về một đối tượng xem bộ nhớ
+
+# min()              Trả về mục nhỏ nhất trong một lần lặp
+
+# next()             Trả về mục tiếp theo trong một lần lặp
+
+# object()           Trả về một đối tượng mới
+
+# oct()              Chuyển đổi một số thành bát phân
+
+# open()             Mở tệp và trả về đối tượng tệp
+
+# ord()              Chuyển đổi một số nguyên đại diện cho Unicode của ký tự được chỉ định
+
+# pow()              Trả về giá trị của x theo lũy thừa của y
+
+# print()            In ra thiết bị đầu ra tiêu chuẩn
+
+# property()         Nhận, thiết lập, xóa một thuộc tính
+
+# range()            Trả về một dãy số, bắt đầu từ 0 và tăng dần 1 (theo mặc định)
+
+# repr()             Trả về phiên bản có thể đọc được của một đối tượng
+
+# Reverse()          Trả về một iterator bị đảo ngược
+
+# round()            Làm tròn số
+
+# set()              Trả về một đối tượng set mới
+
+# setattr()          Đặt một thuộc tính (thuộc tính/phương thức) của một đối tượng
+
+# slice()            Trả về một đối tượng slice
+
+# sorted()           Trả về một danh sách đã được sắp xếp
+
+# staticmethod()     Chuyển đổi một phương thức thành một phương thức tĩnh
+
+# str()              Trả về một đối tượng chuỗi
+
+# sum()              Tính tổng các mục của iterator
+
+# super()            Trả về một đối tượng đại diện cho lớp cha
+
+# tuple()            Trả về một tuple
+
+# type()             Trả về kiểu của một đối tượng
+
+# vars()             Trả về thuộc tính __dict__ của đối tượng
+
+# zip()              Trả về một trình lặp, từ hai trình lặp trở lên
